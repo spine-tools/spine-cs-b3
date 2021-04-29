b4a44a20937ec56fe0eccf09aa2971fb77444ca3
Huang, Jiangyi <jiangyi.huang@vtt.fi>
2021-04-29 14:56:14 +0300

    improve function

--- scripts/modify_SpineOpt_db.py
+++ scripts/modify_SpineOpt_db.py
@@ -71,9 +71,12 @@ def adapt_start_up_costs_of_units(
 
 def modify_generation_capacity_of_units(
         _spineopt_db_export, _unit_name: str, _demand_node_name: str, _new_total_capacity,
-        _new_number_of_units: int = None, _source_node_name: str = None, _alternative='Base'
+        _new_number_of_units: int = None, _source_node_name: str = None,
+        search_alternative='Base', new_alternative: str = 'Base'
 ):
     """
+    :param new_alternative:
+    :param search_alternative:
     :param _spineopt_db_export: an export dictionary for spineDB, obtained via gdx2spinedb.spinedb.export_spinedb()
     :param _unit_name:
     :param _demand_node_name:
@@ -81,20 +84,23 @@ def modify_generation_capacity_of_units(
     :param _new_number_of_units:
     :param _source_node_name: placeholder to specify the unit's source flow node if such a node exists
                               the default name of the node follows f"source_{_unit_name}"
-    :param _alternative:
     :return: an instance of gdx2spinedb.import_ts.SpineDBImporter class
     """
     # initialise a spinedb importer
     _importer_spineopt = SpineDBImporter()
+    if new_alternative != search_alternative:
+        _importer_spineopt.alternatives.append(new_alternative)
 
     _original_db = _spineopt_db_export
     _original_number_of_units = [
         x[3] for x in _original_db['object_parameter_values']
-        if all([x[:3] == ('unit', _unit_name, 'number_of_units'), x[-1] == _alternative])
+        if all([x[:3] == ('unit', _unit_name, 'number_of_units'), x[-1] == search_alternative])
     ][0]
     _original_unit_capacity = [
         x[3] for x in _original_db['relationship_parameter_values']
-        if all([x[:3] == ('unit__to_node', [_unit_name, _demand_node_name], 'unit_capacity'), x[-1] == _alternative])
+        if all([
+            x[:3] == ('unit__to_node', [_unit_name, _demand_node_name], 'unit_capacity'), x[-1] == search_alternative
+        ])
     ][0]
     _original_total_capacity = _original_unit_capacity * _original_number_of_units
 
@@ -107,7 +113,7 @@ def modify_generation_capacity_of_units(
         ]
         if fix_units_on:
             _importer_spineopt.object_parameter_values.append(
-                ('unit', _unit_name, 'fix_units_on', _number_of_units, _alternative)
+                ('unit', _unit_name, 'fix_units_on', _number_of_units, new_alternative)
             )
     else:
         _number_of_units = _original_number_of_units
@@ -116,7 +122,7 @@ def modify_generation_capacity_of_units(
     if _source_node_name:
         _original_source_flow_value = [
             x[3] for x in _original_db['object_parameter_values']
-            if all([x[:3] == ('node', _source_node_name, 'demand'), x[-1] == _alternative])
+            if all([x[:3] == ('node', _source_node_name, 'demand'), x[-1] == search_alternative])
         ][0].to_dict()
 
         _new_source_flow = {
@@ -136,10 +142,10 @@ def modify_generation_capacity_of_units(
         )
 
     _importer_spineopt.object_parameter_values.append(
-        ('unit', _unit_name, 'number_of_units', _number_of_units, _alternative)
+        ('unit', _unit_name, 'number_of_units', _number_of_units, new_alternative)
     )
     _importer_spineopt.relationship_parameter_values.append(
-        ('unit__to_node', [_unit_name, _demand_node_name], 'unit_capacity', _unit_capacity, _alternative)
+        ('unit__to_node', [_unit_name, _demand_node_name], 'unit_capacity', _unit_capacity, new_alternative)
     )
 
     return _importer_spineopt
